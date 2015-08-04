2fc443c338bb8492906573f5868762aa97dec678
Andrew Dang & Alisa Colon <student@epicodus.com>
2015-08-04 09:53:06 -0700

    sleep in second spec pass gives day

--- lib/sleep_in.rb
+++ lib/sleep_in.rb
@@ -1,9 +1,20 @@
 class Time
   define_method(:sleep_in) do
-    if self.saturday?() || self.sunday?()
-      return "It's the weekend, sleep in"
-    else
-      return "Wake up"
+
+    if self.monday?()
+      return "Wake up, it's Monday!"
+    elsif self.tuesday?()
+      return "Wake up, it's Tuesday!"
+    elsif self.wednesday?()
+      return "Wake up, it's Wednesday!"
+    elsif self.thursday?()
+      return "Wake up, it's Thursday!"
+    elsif self.friday?()
+      return "Wake up, it's Friday!"
+    elsif self.saturday?()
+      return "It's Saturday, sleep in"
+    elsif self.sunday?()
+      return "It's Sunday, sleep in"
     end
   end
 end
--- spec/sleep_in_spec.rb
+++ spec/sleep_in_spec.rb
@@ -3,7 +3,10 @@ require("rspec")
 
 describe("Time#sleep_in") do
   it("Find the day of the week given the date") do
-    expect((Time.new(2015, 07, 13).sleep_in())).to(eq("Wake up "))
+    expect((Time.new(2015, 07, 13).sleep_in())).to(eq("Wake up, it's Monday!"))
   end
 
+  it('tells you the day of the week') do
+    expect((Time.new(2015, 07, 11).sleep_in())).to(eq("It's Saturday, sleep in"))
+  end
 end
